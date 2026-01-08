043b951156e1b7b28ac4db646e5d11b8d21dd1bf
PikataProgrammer <petardermendjiev@abv.bg>
2026-01-08 10:51:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Player from "./components/Player";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <main>
+            <div id = "game-container">
+                <ol id = "players">
+                    <Player initialName = "Player1" symbol = "X" />
+                    <Player initialName = "Player2" symbol = "O" />
+                </ol>
+                GAME BOARD
+            </div>
+            LOG
+        </main>
+    )
 }
 
-export default App;
+export default App
